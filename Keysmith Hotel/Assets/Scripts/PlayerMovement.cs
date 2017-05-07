74dffc2a2ad8b59fdc0160ec20ff2a0e810a3a0a
Anthony Williams <anthonyrwilliams91@gmail.com>
2017-05-06 19:25:21 -0600

    gitignore update

--- Keysmith Hotel/Assets/Scripts/PlayerMovement.cs
+++ Keysmith Hotel/Assets/Scripts/PlayerMovement.cs
@@ -18,24 +18,9 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-//		float moveHorizontal = Input.GetAxis ("Horizontal");
-//
-//		Vector2 horizontalMovement = new Vector2 (8 * moveHorizontal, 0.0f);
-//		Vector2 stopMovement = new Vector2 (0.0f, 0.0f);
-//
-//		if (moveHorizontal < .5 && moveHorizontal > -0.5) {
-//
-//			if (rb2d.velocity.x != 0) {
-//				rb2d.velocity = Vector2.zero; 
-//			}
-//
-//		} else if(rb2d.velocity.x <= maxSpeed && rb2d.velocity.x >= maxSpeed * -1){
-//			rb2d.AddForce (horizontalMovement);
-//		}
-//
-//		Debug.Log("Current X-Axis Velocity: " + horizontalMovement.x);
 		Walking();
 	}
+
 
 	void Walking() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
@@ -43,6 +28,7 @@ public class PlayerMovement : MonoBehaviour {
 		Vector2 horizontalMovement = new Vector2 (8 * moveHorizontal, 0.0f);
 		Vector2 stopMovement = new Vector2 (0.0f, 0.0f);
 
+		// CHECKING FOR USER INPUT AND ADJUSTING PLAYER MOVEMENT
 		if (moveHorizontal < .5 && moveHorizontal > -0.5) {
 
 			if (rb2d.velocity.x != 0) {
@@ -54,6 +40,11 @@ public class PlayerMovement : MonoBehaviour {
 		}
 
 		Debug.Log("Current X-Axis Velocity: " + horizontalMovement.x);
+	}
+
+	void Jump() {
+		float moveVertical = Input.GetKeyDown (KeyCode.DownArrow);
+
 	}
 }
 
