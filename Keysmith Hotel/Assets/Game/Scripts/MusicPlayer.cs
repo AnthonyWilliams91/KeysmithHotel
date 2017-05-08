43728789664eb1a431be1704a5c279fa542d4880
Anthony Williams <anthonyrwilliams91@gmail.com>
2017-05-07 19:06:51 -0600

    Music working

---
+++ Keysmith Hotel/Assets/Game/Scripts/MusicPlayer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicPlayer : MonoBehaviour {
+
+
+		void Start()
+		{
+			AudioSource audio = GetComponent<AudioSource>();
+			audio.Play();
+			audio.Play(44100);
+		}
+
+}
